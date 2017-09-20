9d57e7ca2a81601244f80a4bed63c0356b9c391e
Diogo Menezes <diogo.afroruts@gmail.com>
2017-09-20 02:22:52 -0300

    Projeto Fase 2 Finalizado com estilização

--- Projeto/clientes.php
+++ Projeto/clientes.php
@@ -5,37 +5,39 @@ $cliente = $banco->getCliente($_GET['id']);
 ?>
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-</head>
-<body>
-
-<div class="navbar-fixed">
-    <nav>
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo center">Gestão de Clientes</a>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo center">Gestão de Clientes</a>
+            </nav>
         </div>
-    </nav>
-</div>
-<div class="container">
-    <main>
-        <div class="panel panel-primary">
-                <div class="panel-heading"> <?php echo $cliente->getNome()?> </div>
-                <div class="panel-body">
-                        <?php
-                            echo "<b>Nome: </b>".$cliente->getNome()."<br>";
-                            echo "<b>Endereço: </b>".$cliente->getEndereco()."<br>";
-                            echo "<b>CPF: </b>".$cliente->getCPF()."<br>";
-                        ?>
+        <div class="container">
+            <main>
+                <div class="row">
+                    <div class="col s12 m4 l3"><p></p></div>
+                    <div class="col s12 m4 l6"><p></p>
+                            <div class="card-panel teal">
+                                <span class="white-text">
+                                    <?php
+                                    echo "<b>Nome: </b>".$cliente->getNome()."<br>";
+                                    echo "<b>Endereço: </b>".$cliente->getEndereco()."<br>";
+                                    echo "<b>CPF: </b>".$cliente->getCPF()."<br>";
+                                    ?>
+                                </span>
+                                    <a href="index.php" class="btn btn-default">Voltar</a>
+                            </div>
+                    <div class="col s12 m4 l3"><p></p></div>
                     </div>
-                <div class="panel-footer">
-                        <a href="index.php" class="btn btn-default">Voltar</a>
-                    </div>
-            </div>
-    </div>
-</body>
+                </div>
+            </main>
+        </div>
+    </body>
 </html>
